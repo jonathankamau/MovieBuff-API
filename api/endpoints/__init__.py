6648a2bd21789aebb792cf6777f72267945652c0
Jonathan Kamau <jonathan.kamau@andela.com>
2018-09-24 18:50:13 +0300

    add the import for view_favourite_movies

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -2,3 +2,4 @@ from api.endpoints.create_account import CreateAccount
 from api.endpoints.user_login import UserLogin
 from api.endpoints.movie_search import MovieSearch
 from api.endpoints.add_movies import AddMovie
+from api.endpoints.view_favourite_movies import ViewFavouritesList
