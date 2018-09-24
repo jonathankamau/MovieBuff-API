23f0bb85654c9b7064b991ca24af2f05569c36b6
Jonathan Kamau <jonathan.kamau@andela.com>
2018-09-24 12:43:54 +0300

    add import for the addmovie resource in init.py

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -1,3 +1,4 @@
 from api.endpoints.create_account import CreateAccount
 from api.endpoints.user_login import UserLogin
 from api.endpoints.movie_search import MovieSearch
+from api.endpoints.add_movies import AddMovie
