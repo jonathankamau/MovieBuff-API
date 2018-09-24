4f0d96c99f278ab13b7731d9bb77a8c83607e912
Jonathan Kamau <jonathan.kamau@andela.com>
2018-09-24 12:47:05 +0300

    add a get movie query function to the models file

--- api/utils/models.py
+++ api/utils/models.py
@@ -13,6 +13,9 @@ class MyQueries(BaseQuery):
     def get_username(self, user_name):
         return self.filter(self.type.username == user_name)
 
+    def get_movie(self, movie_id):
+        return self.filter(self.type.movie_id == movie_id)
+
 
 class User(db.Document):
     query_class = MyQueries
@@ -25,3 +28,12 @@ class User(db.Document):
     def generate_token(self, expiration=6000):
         serial = Serializer(os.getenv('SECRET'), expires_in=expiration)
         return "Bearer "+serial.dumps({'id': self.password}).decode('utf-8')   
+
+
+class FavouriteMovies(db.Document):
+    query_class = MyQueries
+    movie_id = db.IntField()
+    movie_title = db.StringField()
+    popularity = db.FloatField()
+    release_date = db.DateTimeField()
+    overview = db.StringField()
