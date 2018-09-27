02264613e8e17a1dd18353f2aeeb2d46a6625d37
Jonathan Kamau <jonathan.kamau@andela.com>
2018-09-27 22:08:00 +0300

    fix procfile configuration

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 
 from flask_script import Manager
 from flask_migrate import Migrate
-from app import create_app
+from setup import create_app
 
 movie_app = create_app(environment=os.environ.get('APP_SETTINGS'))
 manager = Manager(movie_app)
