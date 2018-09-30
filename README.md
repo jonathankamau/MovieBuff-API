f35dcc06957ec810d610cc316bc149a6d7aa2dc6
Jonathan Kamau <jonathan.kamau@andela.com>
2018-09-30 05:43:38 +0300

    update all the endpoint url's and the readme

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # MovieBuff-API
 An API that allows users to create, view and update their list of favourite movies!
 
+#### URL endpoints
+
+| URL Endpoint | HTTP Methods | Summary |
+| -------- | ------------- | --------- |
+| `/api/v1/auth/register` | `POST`  | Register a new user|
+|  `/api/v1/auth/login` | `POST` | Login and retrieve token|
+| `/api/v1/movie/search?movie_name={{movie_name}}` | `GET` | Search for a movie by its name |
+| `/api/v1/movie/add` | `POST` | Add a movie to the favourite movies list |
+| `/api/v1/movie/favourites` | `GET` |  Retrieve all movies in favourite movies list |
+| `/api/v1/movie/delete?id={{movie_id}}` | `DELETE` | Delete a movie in the favourite movies list |
+| `/api/v1/user/update` | `PUT` |  Update the logged in user's username |
+
 # Requirements
 Python (3.6)
 Flask (1.0.2)
@@ -34,6 +46,7 @@ Flask (1.0.2)
     This command creates a docker instance for the postgres database as well as for the API and runs the server as well.
 
     Open your browser to http://127.0.0.1:5000/ and you should see the browsable version of the API
+    You can access the api online as well via this link http://movie-buff-api.herokuapp.com/
 
 5. Running tests
 
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restplus import Api
-from api.endpoints import (CreateAccount, UserLogin, MovieSearch, 
-                           AddMovie, ViewFavouritesList, DeleteMovie, 
+from api.endpoints import (CreateAccount, UserLogin, MovieSearch,
+                           AddMovie, ViewFavouritesList, DeleteMovie,
                            UpdateUserDetails)
 from api.utils.models import db
 
@@ -30,14 +30,16 @@ def create_app(environment):
     # enable cross origin resource sharing
     CORS(app)
 
-    api.add_resource(CreateAccount, "/api/user/register", endpoint="register")
-    api.add_resource(UserLogin, "/api/user/login", endpoint="login")
-    api.add_resource(MovieSearch, "/api/movie/search", endpoint="search")
-    api.add_resource(AddMovie, "/api/movie/add", endpoint="add")
+    api.add_resource(CreateAccount, "/api/v1/auth/register",
+                     endpoint="register")
+    api.add_resource(UserLogin, "/api/v1/auth/login", endpoint="login")
+    api.add_resource(MovieSearch, "/api/v1/movie/search", endpoint="search")
+    api.add_resource(AddMovie, "/api/v1/movie/add", endpoint="add")
     api.add_resource(ViewFavouritesList,
-                     "/api/movie/favourites", endpoint="favourites")
-    api.add_resource(DeleteMovie, "/api/movie/delete", endpoint="delete")
-    api.add_resource(UpdateUserDetails, "/api/user/update", endpoint="update")
+                     "/api/v1/movie/favourites", endpoint="favourites")
+    api.add_resource(DeleteMovie, "/api/v1/movie/delete", endpoint="delete")
+    api.add_resource(UpdateUserDetails,
+                     "/api/v1/user/update", endpoint="update")
 
     # handle default 404 exceptions
     @app.errorhandler(404)
