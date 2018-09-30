1befe65fe805c8d20267c860e8eb0c32ac5f850d
Jonathan Kamau <jonathan.kamau@andela.com>
2018-09-30 04:18:41 +0300

    chore(test): add test for view favourite movies list endpoint
    [Finishes #160480643]
    
    Signed-off-by: Jonathan Kamau <jonathan.kamau@andela.com>

--- api/endpoints/view_favourite_movies.py
+++ api/endpoints/view_favourite_movies.py
@@ -1,13 +1,11 @@
 import json
-from flask import g
+from flask import g, jsonify
 from flask_restplus import Resource
 
 from api.utils import token_required
 from api.utils import FavouriteMovies
-from sqlalchemy import inspect
 
 class ViewFavouritesList(Resource):
-
 
     @token_required
     def get(self):
@@ -15,12 +13,23 @@ class ViewFavouritesList(Resource):
         favourite_movies = FavouriteMovies.query.get_user(
             g.current_user.user_id).all()
 
-        for movie in favourite_movies:
-            favourite_list.append({
-                "id": movie.movie_id,
-                "movie_title": movie.movie_title,
-                "description": movie.overview,
-                "popularity": movie.popularity
-            })
-    
-        return favourite_list
+        if favourite_movies:
+            for movie in favourite_movies:
+                favourite_list.append({
+                    "id": movie.movie_id,
+                    "movie_title": movie.movie_title,
+                    "description": movie.overview,
+                    "popularity": movie.popularity
+                })
+                response = jsonify(
+                    message='Your movie list has been retrieved successfully',
+                    list=favourite_list
+                )
+                response.status_code = 200
+        else:
+            response = jsonify(
+                    message='Movie list could not be found or is empty!',
+                )
+            response.status_code = 400
+
+        return response
